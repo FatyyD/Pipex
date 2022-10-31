2de4933aa97beb25b14c2b45922146e94e4512ee
FatyyD <79169041+FatyyD@users.noreply.github.com>
2022-10-31 16:53:31 +0100

    Add files via upload

--- pipex.c
+++ pipex.c
@@ -1,5 +1,15 @@
 #include "pipex.h"
 
+char line_paths(char **envp)
+{
+    char **paths_envp;
+    int i;
+
+    i = 0;
+    while(!ft_strnstr(envp[i],"PATH", 4))
+            i++;
+    return (*envp + 5);
+}
 char    *path(char *cmd, char *ag, char **envp)
 {
     char *paths_envp;
@@ -10,49 +20,59 @@ char    *path(char *cmd, char *ag, char **envp)
     i = 0;
    if (ft_strnstr(cmd, "/", ft_strlen(cmd)))
         return(cmd);
+   paths_envp = line_paths(envp);  
    mypaths = ft_split(paths_envp, ":");
-   mycmdargs = ft_split(ag[2], " ");
+   mycmdargs = ft_split(&ag[2], " ");
    i = -1;
     while(mypaths[++i])
     {
-        mycmdargs
+        cmd = ft_strjoin(mypaths[i], ag[2]);
+
         if(!access(path, F_OK))
         {
         free(mycmdargs);
         }
     }
         exit(EXIT_FAILURE);
-    }
-
 }
 
-void    child_f(int f1, char *cmd1)
+void    child_f(int f1, char *cmd1, char **ag, char **envp)
 {
     int fd[2];
+    char *cmd;
+    int i;
 
     dup2(f1, STDIN_FILENO);
-    dup2(fd[1], STDOUT_FILENO);
-    close(fd[0]);
-    close(f1);
-   /*while(mypaths[++i])
-    {
-        cmd = ft_strjoin(mypaths[i], ag[2]);
-        execve(path(cmd[0], envp), cmd, envp);
-        free(cmd);
-    }
-    exit(EXIT_FAILURE);*/
-}
-
-void  child_s(int f2, char *cmd2)
-{
-    int fd[2];
-    char **cmd;
-
-    dup2(f2, STDIN_FILENO);
     dup2(fd[0], STDOUT_FILENO);
     close(fd[1]);
-    close(f2);
+    close(f1);
+    cmd1 = ft_split(ag[2], ' ');
+    if (cmd && path(cmd, &ag, envp))
+    {
+        execve(path(cmd[0], envp), cmd, envp);
+        perror("Error");
+        free(cmd);
+    }
+    exit(EXIT_FAILURE);
+}
 
+void  child_s(int f2, char *cmd2, char **ag, char **envp)
+{
+    int fd[2];
+    char *cmd;
+
+    dup2(f2, STDIN_FILENO);
+    dup2(fd[1], STDOUT_FILENO);
+    close(fd[0]);
+    close(f2);
+     cmd2 = ft_split(av[3], ' ');
+    if (cmd && path(cmd, &ag, envp))
+    {
+        execve(path(cmd[0], envp), cmd, envp);
+        perror("Error");
+        free(cmd);
+    }
+    exit(EXIT_FAILURE);
     /*if (cmd )
         cmd = ft_strjoin(mypaths[i], ag[2]);
         execve(cmd, path, envp);
@@ -61,7 +81,7 @@ void  child_s(int f2, char *cmd2)
     exit(EXIT_FAILURE);*/
 }
 
-void pipex(int f1, int f2, char *cmd1, char *cmd2)
+void pipex(int f1, int f2, char *cmd1, char *cmd2, char **envp)
 {
     int fd[2];
     int status;
@@ -73,7 +93,7 @@ void pipex(int f1, int f2, char *cmd1, char *cmd2)
     if (c1 < 0)
         return (perror("fork: "));
     if (c1 == 0)
-        child_f(f1, cmd1);
+        child_f(f1, cmd1, ag, envp);
     c2 = fork();
     if (c2 < 0)
         return (perror("fork: "));
@@ -90,13 +110,15 @@ int main(int ac, char **ag, char **envp)
 {
     int f1;
     int f2;
-
+    
+   /* if (ag != 5)
+           return ();*/
     f1 = open(ag[1], O_RDONLY);
     f2 = open(ag[4], O_CREAT | O_RDWR | O_TRUNC, 0644);
     if (f1 < 0 || f2 < 0)
         return (-1);
-        pipex(f1, f2, ag, &envp);
-        return(0);
+    pipex(f1, f2, ag, envp);
+    return(0);
 }
 /*int main(int a, int *argv, char **argc)
 {
--- pipex.h
+++ pipex.h
@@ -11,5 +11,9 @@
 void  child_s(int f2, char *cmd2);
 void pipex(int f1, int f2, char *cmd1, char *cmd2);*/
 int main(int ac, char **ag, char **envp);
+char	**ft_split(char const *s, char c);
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlen(const char *str);
+char	*ft_strnstr(const char *big, const char *little, size_t len);
 
 #endif 
